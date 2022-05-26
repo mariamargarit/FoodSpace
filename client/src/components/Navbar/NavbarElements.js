b71159cdec66d6f9166a2748324bfc60e0f2a29b
tia <tia.margarit@gmail.com>
2022-05-26 14:16:54 +0300

    fix sidebar

--- client/src/components/Navbar/NavbarElements.js
+++ client/src/components/Navbar/NavbarElements.js
@@ -11,6 +11,10 @@ export const Nav = styled.nav`
     position: sticky;
     top: 0;
     z-index: 10;
+
+    @media screen and (max-width: 960px) {
+        transition: 0.8 all ease;
+    }
 `
 export const NavbarContainer = styled.div`
     display: flex;
