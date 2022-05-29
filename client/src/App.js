df0fed25756bc837d7c89425d602d0ec6da843f5
Iuga Stefania <iugastefania26@gmail.com>
2022-05-30 00:41:54 +0300

    sign in functionality

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Home from './pages';
 import SignInPage from "./pages/signin";
 import SignUpPage from "./pages/signup";
 
+//import Navbar from "./components/Navbar";
 
 function App() {
     return (
