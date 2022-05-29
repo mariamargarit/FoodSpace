063ce6ffbd10c35cfe028f470c30bc920c828fb3
Iuga Stefania <iugastefania26@gmail.com>
2022-05-30 01:14:54 +0300

    fixed

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,21 @@ import './App.css';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from './pages';
 import SignInPage from "./pages/signin";
-// import Navbar from "./components/Navbar";
+import SignUpPage from "./pages/signup";
+
+//import Navbar from "./components/Navbar";
 
 function App() {
-  return (
-    <Router>
-        {/*<Navbar />*/}
-        <Routes>
-            <Route path='/' element={<Home/>} exact />
-            <Route path='/signin' element={<SignInPage/>} exact />
-        </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            {/*<Navbar />*/}
+            <Routes>
+                <Route path='/' element={<Home/>} exact/>
+                <Route path='/signin' element={<SignInPage/>} exact/>
+                <Route path='/signup' element={<SignUpPage/>} exact/>
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
