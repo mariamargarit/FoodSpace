8207c1cf4239451be9712f3c0a8089214b899741
tia <tia.margarit@gmail.com>
2022-05-31 00:01:16 +0300

    add discover data

--- client/src/components/InfoSection/Data.js
+++ client/src/components/InfoSection/Data.js
@@ -2,11 +2,24 @@ export const homeObjOne = {
   id: 'about',
   lightBg: false,
   lightText: true,
-  lightTextDesc: true,
+  darkText: false,
   topLine: 'Restaurant picker',
-  headline: 'Find the restaurant for you',
+  headline: 'Find the best restaurant for you',
   description: 'Just answer the questions provided and the app will choose the most suitable restaurant based on your preferences.',
   imgStart: false,
   img: require('../../assets/svg-1.svg').default,
-  alt: 'Restaurant',
+  alt: 'Food Order App',
+}
+
+export const homeObjTwo = {
+  id: 'discover',
+  lightBg: true,
+  lightText: false,
+  darkText: true,
+  topLine: 'Personalized results',
+  headline: 'Make the first step in your order process',
+  description: 'Get your answer in minutes from our extended list',
+  imgStart: true,
+  img: require('../../assets/svg-2.svg').default,
+  alt: 'People Talking',
 }
